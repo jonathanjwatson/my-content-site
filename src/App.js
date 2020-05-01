2e97eb72fc3f0cff537f8fa0d3c1ad7b2c6b0285
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-05-01 12:57:28 -0400

    setup react-router-dom and basic 3 page routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/Shared/NavBar";
+import Home from "./containers/Home";
+import SignIn from "./containers/SignIn";
+import Content from "./containers/Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/content" component={Content} />
+    </Router>
   );
 }
 
---
+++ src/containers/SignIn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is my sign in page</h1>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
